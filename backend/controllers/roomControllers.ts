f3901704fecce3fe347c9cb07f905dd54ed829ad
Damion Wilson <dwilson_wbs@protonmail.com>
2024-10-31 19:42:33 -0400

    added route to display specific room

--- backend/controllers/roomControllers.ts
+++ backend/controllers/roomControllers.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import Room from "../models/room";
 
+// Display all rooms
 export const allRooms = async (req: NextRequest) => {
   const resPerPage: number = 8;
   const rooms = await Room.find();
@@ -11,9 +12,31 @@ export const allRooms = async (req: NextRequest) => {
   });
 };
 
+// Create a new room
 export const newRoom = async (req: NextRequest) => {
   const body = await req.json();
   const room = await Room.create(body);
+  return NextResponse.json({
+    success: true,
+    room,
+  });
+};
+
+// Search for and display details of a single room
+export const getRoomDetails = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const room = await Room.findById(params.id);
+
+  if (!room) {
+    return NextResponse.json(
+      {
+        message: "Room not found",
+      },
+      { status: 404 }
+    );
+  }
   return NextResponse.json({
     success: true,
     room,
