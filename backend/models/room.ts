5b8c64573ed6d3c87d2398437486ae034bb3134c
Damion Wilson <dwilson_wbs@protonmail.com>
2024-10-31 09:13:45 -0400

    fixed bug connecting to database

--- backend/models/room.ts
+++ backend/models/room.ts
@@ -153,7 +153,7 @@ const roomSchema: Schema = new Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-    required: true,
+    required: false,
   },
   createdAt: {
     type: Date,
