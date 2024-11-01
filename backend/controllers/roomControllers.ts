b235251fbb296106e3283aa9e076f17bb7c629dd
Damion Wilson <dwilson_wbs@protonmail.com>
2024-10-31 22:53:26 -0400

    set up the admin and public routes

--- backend/controllers/roomControllers.ts
+++ backend/controllers/roomControllers.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import Room from "../models/room";
 
-// Display all rooms
+// Display all rooms => /api/rooms
 export const allRooms = async (req: NextRequest) => {
   const resPerPage: number = 8;
   const rooms = await Room.find();
@@ -12,7 +12,7 @@ export const allRooms = async (req: NextRequest) => {
   });
 };
 
-// Create a new room
+// Create a new room => /api/admin/rooms
 export const newRoom = async (req: NextRequest) => {
   const body = await req.json();
   const room = await Room.create(body);
@@ -22,7 +22,7 @@ export const newRoom = async (req: NextRequest) => {
   });
 };
 
-// Search for and display details of a single room
+// Get room details => /api/rooms/:id
 export const getRoomDetails = async (
   req: NextRequest,
   { params }: { params: { id: string } }
@@ -40,5 +40,55 @@ export const getRoomDetails = async (
   return NextResponse.json({
     success: true,
     room,
+  });
+};
+
+// Update room details => /api/admin/rooms/:id
+export const updateRoom = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  let room = await Room.findById(params.id);
+  const body = await req.json();
+
+  if (!room) {
+    return NextResponse.json(
+      {
+        message: "Room not found",
+      },
+      { status: 404 }
+    );
+  }
+  room = await Room.findByIdAndUpdate(params.id, body, {
+    new: true,
+  });
+
+  return NextResponse.json({
+    success: true,
+    room,
+  });
+};
+
+// Delete room details => /api/admin/rooms/:id
+export const deleteRoom = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const room = await Room.findById(params.id);
+
+  if (!room) {
+    return NextResponse.json(
+      {
+        message: "Room not found",
+      },
+      { status: 404 }
+    );
+  }
+
+  // TODO - Delete images associated with the room
+
+  await room.deleteOne();
+  return NextResponse.json({
+    success: true,
   });
 };
