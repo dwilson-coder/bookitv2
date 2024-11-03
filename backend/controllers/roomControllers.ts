e5055adefabf3209151ae6698132d5daf5abf063
Damion Wilson <dwilson_wbs@protonmail.com>
2024-11-03 10:59:46 -0500

    added the search by location feature

--- backend/controllers/roomControllers.ts
+++ backend/controllers/roomControllers.ts
@@ -1,13 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
-import Room from "../models/room";
+import Room, { IRoom } from "../models/room";
 import ErrorHandler from "../utils/errorHandler";
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors";
+import APIFilters from "../utils/apiFilters";
 
 // Get all rooms  =>  /api/rooms
 export const allRooms = catchAsyncErrors(async (req: NextRequest) => {
   const resPerPage: number = 8;
 
-  const rooms = await Room.find();
+  // const rooms = await Room.find();
+  const { searchParams } = new URL(req.url);
+
+  const queryStr: any = {};
+
+  searchParams.forEach((value, key) => {
+    queryStr[key] = value;
+  });
+
+  const apiFilters = new APIFilters(Room, queryStr).search();
+  const rooms: IRoom[] = await apiFilters.query;
 
   return NextResponse.json({
     success: true,
